e70725b6e7b6c6ee11693a175532e45bb51058d0
Hendrik Elvers <hendrik11ers@msn.com>
2021-12-07 14:57:04 +0100

    200 episodes

--- mainTorch.py
+++ mainTorch.py
@@ -23,13 +23,13 @@ WEIGHT_ATTN     = 1.0
 # Weight for the motion primitive weight loss
 WEIGHT_W        = 50.0
 # Weight for the trajectroy generation loss
-WEIGHT_TRJ      = 1.0
+WEIGHT_TRJ      = 5.0
 # Weight for the time progression loss
 WEIGHT_DT       = 14.0
 # Weight for the phase prediction loss
 WEIGHT_PHS      = 1.0
 # Number of epochs to train
-TRAIN_EPOCHS    = 1000
+TRAIN_EPOCHS    = 200
 
 
 def count_parameters(model):
