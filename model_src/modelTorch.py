a6630a9c326e08420ecededce59fc6f5729a404d
Hendrik Elvers <h.elvers@synergeticon.com>
2022-05-27 19:20:10 +0200

    asd

--- model_src/modelTorch.py
+++ model_src/modelTorch.py
@@ -270,6 +270,7 @@ class PolicyTranslationModelTorch(nn.Module):
             #self.plan_nn = TransformerUpConv(model_setup).to(inpt_features.device)
             model_setup['ntoken'] = inpt_features.size(-1)
             self.plan_nn = TransformerModel(model_setup=self.model_setup['plan_nn']['plan']).to(inpt_features.device)
+        print(in_transformer)
         plan = self.plan_nn.forward(in_transformer) #350x16x8
         #print(f'plan sape: {plan.shape}')
 
